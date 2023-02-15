96834ca17749729922aaf1a95c43b76868c46d91
44013031 <121196625+44013031@users.noreply.github.com>
2023-02-15 03:48:04 +0000

    2) strings forms 3) returning it but in a form with variables 4) ab returns abolute value of #, confused on how to print

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 class Vector2d:
-    pass
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y 
+
+    def __reper__(self):
+        return f"Vector 2D(x = {self.x},y-{self.y}"
+        
+    def __str__(self):
+        return f"Vector 2D({self.x}i + {self.y}j"
+
+    def __abs__(self):
+        lenght = self.x**2 + self.y**2 
+      return(lenght)
+
