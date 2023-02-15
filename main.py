cbf63fc30d8f9be244f7721fedd51609a00708f3
EduardCulajay76 <121196494+EduardCulajay76@users.noreply.github.com>
2023-02-15 04:02:43 +0000

    Init, Repr, & Str, methods finished

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 class Vector2d:
-    pass
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Vector2d(x={self.x},y={self.y})'
+    
+    def __str__(self):
+        return f'{str(self.x)}i + {str(self.y)}j'
