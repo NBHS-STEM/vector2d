748d6d0ddfb3a823cd400895d61091947f9622b7
14197 <118582010+14197@users.noreply.github.com>
2023-02-16 17:24:57 +0000

    Definitely did lots of work today. Did NOT play games or scroll aimlessly on my phone.

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 class Vector2d:
-    pass
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Vector2d(x={self.x},y = {self.y})"
+
+    def __str__(self):
+        return f"{self.x}i + {self.y}j"
+
+    def __abs__(self):
+        return (self.x**2 + self.y**2)**.5
+    
+    def __neg__(self):
+        return f"Vector(-{self.x},-{self.y})"
+    
+    def __add__(self,other):
+        return f"Vector2d({int(self.x) + int(other.x)},{int(self.y) + int(other.y)})"
