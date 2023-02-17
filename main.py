1a1c3dac977b3a606c109d964d53b39d7eee2154
EduardCulajay76 <121196494+EduardCulajay76@users.noreply.github.com>
2023-02-17 01:01:15 +0000

    Completed Add Function

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ class Vector2d:
 
     def __repr__(self):
         return f'Vector2d(x={self.x},y={self.y})'
-    
     def __str__(self):
         return f'{str(self.x)}i + {str(self.y)}j'
-    
     def __abs__(self):
         return ((self.x)**2 + (self.y)**2)**(1/2)
-    
     def __neg__(self):
         return f'Vector2d(-{self.x},-{self.y})'
+    def __add__(self,other):
+        return f'Vector2d({self.x+other.x},{self.y+other.y})'
