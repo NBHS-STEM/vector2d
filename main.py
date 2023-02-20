6b9d58037468ad95004c8eef49e350a71611626e
WilliamV1211 <121183887+WilliamV1211@users.noreply.github.com>
2023-02-20 13:48:59 +0000

    Classwork for Vector2d class

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ class Vector2d:
         return f"{self.x}i + {self.y}j"
     def __abs__(self):   
         c = self.x ** 2 + self.y ** 2
-        return c ** (1/2)
+        return c ** (1/2) 
+    def __neg__(self): 
+        return f"Vector2d(-{self.x}, -{self.y})" 
+    def __add__(self, other): 
+        sum_x = self.x + other.x 
+        sum_y = self.y + other.y 
+        return f"Vector2d({sum_x},{sum_y})"
     
-test = Vector2d(3,4)
+    
+test = Vector2d(3,4) 
+test1 = Vector2d(1,-1)
