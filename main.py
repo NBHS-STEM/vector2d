b06a9621a3070e8919316df36f85c3aa297aba5f
EduardCulajay76 <121196494+EduardCulajay76@users.noreply.github.com>
2023-02-21 03:07:14 +0000

    Fixed add Function / Added spaces between function

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ class Vector2d:
 
     def __repr__(self):
         return f'Vector2d(x={self.x},y={self.y})'
+    
     def __str__(self):
         return f'{str(self.x)}i + {str(self.y)}j'
+    
     def __abs__(self):
         return ((self.x)**2 + (self.y)**2)**(1/2)
+    
     def __neg__(self):
-        return f'Vector2d(-{self.x},-{self.y})'
+        return f'Vector2d({self.x * (-1)},{self.y * (-1)})'
+    
     def __add__(self,other):
         return f'Vector2d({self.x+other.x},{self.y+other.y})'
