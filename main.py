6b088b0a63c7cc69870ced6d9dcbf0ee1d629cc6
HusseinAWAWADA <121184339+HusseinAWAWADA@users.noreply.github.com>
2023-02-21 16:24:53 +0000

    your msg

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ class Vector2d:
         return f"{self.x}I+{self.y}J"
     def __abs__(self):
         return ((self.x**2)+(self.y**2)) **(1/2) 
-    
+    def __neg__(self):
+        return Vector2d(self.x*-1, self.y*-1)
+    def __add__(self,rest_of_x): 
+        return Vector2d((self.x+rest_of_x.x), (self.y+rest_of_x.y))
