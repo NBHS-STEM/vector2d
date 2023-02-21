124ec197c3a9f678bc90d168a51ca12ccd5539a5
44004098 <121195860+44004098@users.noreply.github.com>
2023-02-21 13:39:50 +0000

    I defined a class with functions, repr, str, abs, neg, and add

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 class Vector2d:
-    pass
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Vector2d(x={self.x},y={self.y}) is True"
+
+    def __str__(self):
+        return f"{self.x}i,{self.y}j"
+
+    def __abs__(self):
+        hypotenuse = (self.x**2+self.y**2)**.5
+        return hypotenuse 
+
+    def __neg__(self):
+        return Vector2d(self.x*-1, self.y*-1)
+    
+    def __add__(self,other):
+        return Vector2d((self.x + other.x),(self.y + other.y))
