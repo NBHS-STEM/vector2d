2c4944b34f3e4b307dac29634880e1436b2aa5fa
Max-Contreras <121184009+Max-Contreras@users.noreply.github.com>
2023-02-21 16:20:59 +0000

    Deleted spaced

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ class Vector2d:
     def __str__(self):
         return f"{self.x}i + {self.y}j"
     def __abs__(self):
-        return  ((self.x**2)+(self.y**2))**(1/2)
+        return ((self.x**2)+(self.y**2))**(1/2)
     def __neg__(self):
-        return f"Vector2d({self.x}*-1,{self.y}*-1)"
+        return Vector2d(self.x*-1,self.y*-1)
     def __add__(self, other):
         return Vector2d(self.x + other.x, self.y + other.y)
