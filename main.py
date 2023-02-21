726d4522d1d054089e9e31ce236100c309112853
44013031 <121196625+44013031@users.noreply.github.com>
2023-02-21 02:38:57 +0000

    fixed neg method

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ class Vector2d:
 
     def __repr__(self):
         return f"Vector2d(x={self.x},y={self.y})"
+    
     def __str__(self):
         return f"{str(self.x)}i + {str(self.y)}j"
 
-    def __abs__(self):
+    def __abs__(self): 
         lenght = (self.x**2 + self.y**2 ) **  (1/2)
         return lenght
 
     def __neg__(self):  
-        return f"Vector2d(-{self.x},-{self.y})"
+        return Vector2d(self.x*-1,self.y*-1)
+    ;
     def __add__(self,other):
         return f"Vector2d({self.x+other.x},{self.y+other.y})"
