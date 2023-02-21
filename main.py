ef0469dbbac3fb09521db7baa17771189680447f
Bryant-Gonzalez <121184243+Bryant-Gonzalez@users.noreply.github.com>
2023-02-21 14:50:14 +0000

    Vector2d class

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 class Vector2d:
-    pass
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return f"Vector2d(x={self.x},y={self.y})"
+    def __str__(self):
+        return f"{self.x}i + {self.y} j"
+    def __abs__(self):
+        return ((self.x **2)+(self.y**2))**(1/2)
+    def __neg__(self):
+        return Vector2d(self.x*-1,self.y*-1)   
+    def __add__(self,other):
+        return Vector2d((self.x + other.x),(self.y + other.y))
