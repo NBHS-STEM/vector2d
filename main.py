b9f43016620908b5d8ffa9a45e35240a8280e0d2
HusseinAWAWADA <121184339+HusseinAWAWADA@users.noreply.github.com>
2023-02-21 16:06:17 +0000

    Save solutions for 2/16

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 class Vector2d:
-    pass
+    def __init__(self, x, y):
+        self.x= x
+        self.y= y 
+    def __repr__(self):
+        return f"Vector2d(x='{self.x}',y='{self.y}')"
+    def __str__(self):
+        return f"{self.x}I+{self.y}J"
+    def __abs__(self):
+        return ((self.x**2)+(self.y**2)) **(1/2) 
+    
