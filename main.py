28d981ce163f8af5a53f5fb394abf8e7c9377d0b
Jerraldy <121196050+Jerraldy@users.noreply.github.com>
2023-02-23 17:03:32 +0000

    Work

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 class Vector2d:
-    pass
+    def _init_(self, x, y):
+         self.x =x
+         self.y =y
+   
+    def _repr_(self):
+        return f"Vecto2d(x={self.x}, y={self.y}) is True"
+    
+    def _str_(self):
+        return f"{self.x}i, {self.y}j"
+    
+    def _abs_(self):
+        hypotenuse = (self.x**2+self.y**2)**.5
+        
+    
+    def __eq__(self):
+        if Vector2d((self.x, self.y)==(other.x,other.y))
+    def_sub_(self):
+        if vector2d((self.x-other.y), (self.x-other.y))
