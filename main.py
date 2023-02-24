be776099f9af56ccee1fa6f8aa20e4820236ba9a
divadn <121898480+divadn@users.noreply.github.com>
2023-02-24 20:09:28 +0000

    HW submission for 2/23

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import math
 class Vector2d:
-    pass
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def __repr__(self):
+        return f'Vector2d(x = {self.x}, y = {self.y})'
+    def __str__(self): 
+        return f'{self.x}i + {self.y}j' 
+    def __abs__(self): 
+        return ((self.x**2) + (self.y**2))**(0.5)
+    def __neg__(self):
+        return Vector2d(self.x*-1, self.y*-1)
+    def __add__(self,other):
+        return Vector2d((self.x + other.x), (self.y + other.y))
+    def __eq__(self, other):
+        return self.x == other.x 
+    def __sub__(self,other):
+        return Vector2d((self.x - other.x), (self.y - other.y)) 
+    def angle(self):
+        return math.atan2(self.y) , (self.x)  
+         
+
+        
