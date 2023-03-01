442a0d6fd614f9de4c449af31b27457920790a31
WilliamV1211 <121183887+WilliamV1211@users.noreply.github.com>
2023-03-01 16:26:13 +0000

    Edited and completed print_conversion function assignment from 2/28

--- flash_cards.py
+++ flash_cards.py
@@ -1,9 +1,10 @@
+from vector2d import Vector2d
 def print_conversion(vector, str): 
     if str == "polar":  
-        print(30 * "-")
-        print(f"Convert {vector.x}i + {vector.y}j to polar form") 
-        print(30 * "-")
+        print(40 * "-")  
+        print(f"Convert {vector.x}i + {vector.y}j to polar form")
+        print(40 * "-")
     elif str == "rectangular":  
-        print(30 * "-")
+        print(40 * "-")
         print(f"Convert <{abs(vector)}, {vector.angle()}> to rectangular form") 
-        print(30 * "-")
+        print(40 * "-")
