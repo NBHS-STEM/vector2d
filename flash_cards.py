87dd3d3e5eddf3ec97e082aa0069de43aca413b7
WilliamV1211 <121183887+WilliamV1211@users.noreply.github.com>
2023-03-06 12:49:25 +0000

    Completed work from 3/1 and added 2 functions to flash_cards.py

--- flash_cards.py
+++ flash_cards.py
@@ -1,3 +1,4 @@
+import random
 from vector2d import Vector2d
 def print_conversion(vector, str): 
     if str == "polar":  
@@ -7,4 +8,15 @@ def print_conversion(vector, str):
     elif str == "rectangular":  
         print(40 * "-")
         print(f"Convert <{abs(vector)}, {vector.angle()}> to rectangular form") 
-        print(40 * "-")
+        print(40 * "-") 
+    
+def generate_vector():  
+    random_x = random.randint(-20,21) 
+    random_y = random.randint(-20,21)
+    return Vector2d(random_x, random_y) 
+
+def print_conversion_solution(vector, str): 
+    if str == "polar": 
+        print(f"<{abs(vector)}, {vector.angle()}>") 
+    elif str == "rectangular": 
+        print(f"{vector.x}i + {vector.y}j")
